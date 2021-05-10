3efdd092032df6020629ce8ca84252760a405d34
Mateusz Nowakowski <mat13mn@gmail.com>
2021-05-10 08:55:21 +0200

    implement extrinsic shuffler

---
+++ client/shuffler/src/lib.rs
@@ -0,0 +1,64 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+use sp_runtime::traits::{Block as BlockT, BlakeTwo256};
+use sp_runtime::generic::BlockId;
+use sp_runtime::AccountId32;
+use sp_api::{ApiRef, ProvideRuntimeApi, Encode, HashT};
+use extrinsic_info_runtime_api::runtime_api::ExtrinsicInfoRuntimeApi;
+use sp_std::collections::vec_deque::VecDeque;
+use sp_std::collections::btree_map::BTreeMap;
+use sp_std::vec::Vec;
+use sp_core::crypto::Ss58Codec;
+// , HashMap};
+use rand::rngs::StdRng;                                                                                 
+use rand::prelude::SliceRandom;
+
+
+pub fn shuffle<'a, Block, Api>(api: &ApiRef<'a, Api::Api>, block_id: &BlockId<Block>, extrinsics: Vec<Block::Extrinsic>, mut seed: StdRng) -> Vec<Block::Extrinsic> where
+Block: BlockT,
+Api: ProvideRuntimeApi<Block> + 'a,
+Api::Api: ExtrinsicInfoRuntimeApi<Block>
+
+{
+
+    log::debug!(target: "block_shuffler", "origin order:");
+    let mut grouped_extrinsics: BTreeMap<Option<AccountId32>, VecDeque<_>> = extrinsics
+        .into_iter()
+        .fold(BTreeMap::new(), |mut groups, tx| {
+            let tx_hash = BlakeTwo256::hash(&tx.encode());
+            let who = api.get_info(block_id, tx.clone()).unwrap_or(None).map(|info| Some(info.who)).unwrap_or(None);
+
+            log::debug!(target: "block_shuffler", "who:{:32}  extrinsic:{:?}",who.clone().map(|x| x.to_ss58check()).unwrap_or_else(|| String::from("None")), tx_hash);
+            
+            groups.entry(who).or_insert(VecDeque::new()).push_back(tx);
+            groups
+        });
+
+    // generate exact number of slots for each account
+    // [ Alice, Alice, Alice, ... , Bob, Bob, Bob, ... ]
+    let mut slots: Vec<_> = grouped_extrinsics
+        .iter()
+        .map(|(who, txs)| vec![who;txs.len()])
+        .flatten()
+        .map(|elem| elem.to_owned())
+        .collect();
+
+    // shuffle slots
+    slots.shuffle(&mut seed);
+
+    // fill slots using extrinsics in order
+    // [ Alice, Bob, ... , Alice, Bob ]
+    //              ↓↓↓
+    // [ AliceExtrinsic1, BobExtrinsic1, ... , AliceExtrinsicN, BobExtrinsicN ]
+    let shuffled_extrinsics: Vec<_> = slots
+        .into_iter()
+        .map(|who| grouped_extrinsics.get_mut(&who).unwrap().pop_front().unwrap())
+        .collect();
+
+    log::debug!(target: "block_shuffler", "shuffled order");
+    for tx in shuffled_extrinsics.iter() {
+            let tx_hash = BlakeTwo256::hash(&tx.encode());
+            log::debug!(target: "block_shuffler", "extrinsic:{:?}", tx_hash);
+    }
+
+    shuffled_extrinsics
+}

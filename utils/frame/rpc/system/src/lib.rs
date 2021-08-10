980866725f8a9dca1b80cef3b786b7c382de8a29
Striker <siddiquishoeb.iitm@gmail.com>
2021-08-10 10:21:44 +0530

    Compile failing

--- utils/frame/rpc/system/src/lib.rs
+++ utils/frame/rpc/system/src/lib.rs
@@ -27,15 +27,20 @@ use jsonrpc_core::{
 };
 use jsonrpc_derive::rpc;
 use sc_client_api::light::{future_header, Fetcher, RemoteBlockchain, RemoteCallRequest};
+use sc_client_api::{
+	client::{BlockBackend},
+};
 use sc_rpc_api::DenyUnsafe;
 use sp_block_builder::BlockBuilder;
 use sp_blockchain::{Error as ClientError, HeaderBackend};
 use sp_core::{hexdisplay::HexDisplay, Bytes};
 use sp_runtime::{generic::BlockId, traits};
 use sp_transaction_pool::{InPoolTransaction, TransactionPool};
+use sp_api::{ApiExt, ApiRef, ProvideRuntimeApi, TransactionOutcome};
 
 pub use self::gen_client::Client as SystemClient;
 pub use frame_system_rpc_runtime_api::AccountNonceApi;
+use extrinsic_info_runtime_api::runtime_api::ExtrinsicInfoRuntimeApi;
 
 /// Future that resolves to account nonce.
 pub type FutureResult<T> = Box<dyn Future<Item = T, Error = RpcError> + Send>;
@@ -93,24 +98,26 @@ impl<P: TransactionPool, C, B> FullSystem<P, C, B> {
     }
 }
 
-impl<P, C, Block, AccountId, Index> SystemApi<<Block as traits::Block>::Hash, AccountId, Index>
+impl<'a, P, C, Block, AccountId, Index> SystemApi<<Block as traits::Block>::Hash, AccountId, Index>
     for FullSystem<P, C, Block>
 where
     C: sp_api::ProvideRuntimeApi<Block>,
     C: HeaderBackend<Block>,
+    C: BlockBackend<Block>,
     C: Send + Sync + 'static,
     C::Api: AccountNonceApi<Block, AccountId, Index>,
     C::Api: BlockBuilder<Block>,
+    C::Api: ExtrinsicInfoRuntimeApi<Block>,
     P: TransactionPool + 'static,
     Block: traits::Block,
-    AccountId: Clone + std::fmt::Display + Codec,
+    AccountId: Clone + std::fmt::Display + Codec + Decode + std::cmp::PartialEq,
     Index: Clone + std::fmt::Display + Codec + Send + traits::AtLeast32Bit + 'static,
 {
     fn nonce(&self, account: AccountId) -> FutureResult<Index> {
         let get_nonce = || {
             let api = self.client.runtime_api();
-            let best = self.client.info().best_hash;
-            let at = BlockId::hash(best);
+            let best_hash = self.client.info().best_hash;
+            let at = BlockId::hash(best_hash);
 
             let nonce = api
                 .account_nonce(&at, account.clone())
@@ -120,7 +127,16 @@ where
                     data: Some(format!("{:?}", e).into()),
                 })?;
 
-            Ok(adjust_nonce(&*self.pool, account, nonce))
+            let best_block_extrinsics: Vec<Block::Extrinsic> =
+                self.client.block_body(&at)
+                    .map_err(|e| RpcError {
+                    code: ErrorCode::ServerError(Error::RuntimeError.into()),
+                    message: "Failed to get parent blocks extrinsics.".into(),
+                    data: Some(format!("{:?}", e).into()),
+                })?
+                .unwrap_or_default();
+
+            Ok(adjust_nonce::<P, C, AccountId, Index, Block>(&*self.pool, account, nonce, best_block_extrinsics, at, &api))
         };
 
         Box::new(result(get_nonce()))
@@ -195,7 +211,7 @@ where
     C: Send + Sync + 'static,
     F: Fetcher<Block> + 'static,
     Block: traits::Block,
-    AccountId: Clone + std::fmt::Display + Codec + Send + 'static,
+    AccountId: Clone + std::fmt::Display + Codec + Send + 'static + Decode + std::cmp::PartialEq,
     Index: Clone + std::fmt::Display + Codec + Send + traits::AtLeast32Bit + 'static,
 {
     fn nonce(&self, account: AccountId) -> FutureResult<Index> {
@@ -231,8 +247,14 @@ where
             data: Some(format!("{:?}", e).into()),
         });
 
+        #[allow(unused_variables)]
         let pool = self.pool.clone();
-        let future_nonce = future_nonce.map(move |nonce| adjust_nonce(&*pool, account, nonce));
+
+        // // FIXME Fetch block extrinsics rather than use default. 
+        // let best_block_extrinsics: Vec<Block::Extrinsic> =
+        //         Default::default();
+        // // FIXME Pass in the api?
+        // let future_nonce = future_nonce.map(move |nonce| adjust_nonce::<P, C, AccountId, Index, Block>(&*pool, account, nonce, best_block_extrinsics, best_id, ));
 
         Box::new(future_nonce)
     }
@@ -252,21 +274,45 @@ where
 
 /// Adjust account nonce from state, so that tx with the nonce will be
 /// placed after all ready txpool transactions.
-fn adjust_nonce<P, AccountId, Index>(pool: &P, account: AccountId, nonce: Index) -> Index
+fn adjust_nonce<'a, P, C, AccountId, Index, Block>(pool: &P, account: AccountId, nonce: Index, block_extrinsics: Vec<Block::Extrinsic>, block_id: BlockId<Block>, api: &ApiRef<'a, C::Api>) -> Index
 where
     P: TransactionPool,
-    AccountId: Clone + std::fmt::Display + Encode,
+    AccountId: Clone + std::fmt::Display + Encode + Decode + std::cmp::PartialEq,
     Index: Clone + std::fmt::Display + Encode + traits::AtLeast32Bit + 'static,
+    Block: traits::Block,
+    C: ProvideRuntimeApi<Block> + 'a,
+    C::Api: ExtrinsicInfoRuntimeApi<Block>,
 {
     log::debug!(target: "rpc", "State nonce for {}: {}", account, nonce);
+
+    let mut current_nonce = nonce.clone();
+
+    for tx in block_extrinsics.into_iter(){
+        let (tx_who, tx_nonce): (sp_runtime::AccountId32, u32) = api.execute_in_transaction(|api| {
+            // store deserialized data and revert state modification caused by 'get_info' call
+            match api.get_info(&block_id, tx.clone()){
+                Ok(result) => TransactionOutcome::Rollback(result),
+                Err(_) => TransactionOutcome::Rollback(None)
+            }
+        })
+        // This should always unwrap and never or_else, as it is expected to.
+        // Otherwise nonce for AccountId32=0 will be miscalculated as 0
+        .map_or_else( || Default::default(), |info| (info.who, info.nonce));
+
+        if (<AccountId>::decode(&mut &<[u8; 32]>::from(tx_who)[..]).unwrap() == account) && (Index::from(tx_nonce) == current_nonce) {
+            current_nonce += traits::One::one();
+        }
+
+    };
+
     // Now we need to query the transaction pool
     // and find transactions originating from the same sender.
     //
     // Since extrinsics are opaque to us, we look for them using
     // `provides` tag. And increment the nonce if we find a transaction
     // that matches the current one.
-    let mut current_nonce = nonce.clone();
-    let mut current_tag = (account.clone(), nonce).encode();
+    
+    let mut current_tag = (account.clone(), current_nonce.clone()).encode();
     for tx in pool.ready() {
         log::debug!(
             target: "rpc",

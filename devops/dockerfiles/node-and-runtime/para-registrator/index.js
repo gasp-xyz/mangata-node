751c28bf9a5bbd6fa3c1fa3d33a5de82db990ec8
Mateusz Nowakowski <mat13mn@gmail.com>
2021-12-30 09:43:20 +0100

    allow to pass parachain genesis configuration throught environment variables

--- devops/dockerfiles/node-and-runtime/para-registrator/index.js
+++ devops/dockerfiles/node-and-runtime/para-registrator/index.js
@@ -5,12 +5,12 @@ import { ApiPromise, WsProvider } from '@polkadot/api';
 import BN from "bn.js";
 import fs from "fs";
 
+const state_file = process.env.STATE_FILE ? process.env.STATE_FILE : "/code/genesis-state";
+const wasm_file = process.env.WASM_FILE ? process.env.WASM_FILE : "/code/genesis-wasm";
 
 
 async function main () {
     const wsUrl = "ws://10.0.0.2:9944";
-    const paraId = 2000;
-    const pathToFiles = "/tmp/";
 
     const wsProvider = new WsProvider(wsUrl);
     const api = await ApiPromise.create({
@@ -19,13 +19,7 @@ async function main () {
 
     let keyring = new Keyring({type: "sr25519"});
     const alice = keyring.addFromUri('//Alice');
-    // sudo = new User(keyring, sudoUserName);
-    // testUser1 = new User(keyring, user);
     console.info("get para id");
-    // add users to pair.
-    // keyring.addPair(testUser1.keyRingPair);
-    // keyring.addPair(sudo.keyRingPair);
-    //await signTx(api, api.tx.registrar.reserve(), testUser1.keyRingPair);
     const nextParaIdBefore = new BN(
       await (await api.query.registrar.nextFreeParaId()).toString()
     );
@@ -43,9 +37,9 @@ async function main () {
       await (await api.query.registrar.nextFreeParaId()).toString()
     );
     const genesis = fs
-      .readFileSync("/code/genesis-state")
+      .readFileSync(state_file)
       .toString();
-    const wasm = fs.readFileSync("/code/genesis-wasm").toString();
+    const wasm = fs.readFileSync(wasm_file).toString();
 
     console.info("get para id 5");
     const scheduleParaInit = api.tx.parasSudoWrapper.sudoScheduleParaInitialize(
@@ -57,15 +51,7 @@ async function main () {
       }
     );
     console.info("registering para id");
-    const unsub = await api.tx.sudo.sudo(scheduleParaInit).signAndSend(alice, async ({ events = [] , status, dispatchError }) => {
-        console.info(status.toHuman());
-    });
-
-    //         const unsub = await tx.signAndSend(this.signer, options, async ({ events = [] , status, dispatchError }) => {
-    //             if (status.isInBlock || status.isFinalized) {
-    // console.info(result.toHuman());
-    // console.info("para id registered");
-    // // await waitNewBlock();
+    await api.tx.sudo.sudo(scheduleParaInit).signAndSend(alice);
   };
 
 

168c3767132e0927085f68d118169534fe4b5ced
Mateusz Nowakowski <mat13mn@gmail.com>
2022-03-17 18:20:52 +0100

    benchmark for sell_asset

--- pallets/xyk/src/benchmarking.rs
+++ pallets/xyk/src/benchmarking.rs
@@ -68,43 +68,20 @@ benchmarks! {
 
 	sell_asset {
 		let caller: T::AccountId = whitelisted_caller();
-		let first_asset_amount = MILION;
-		let second_asset_amount = MILION;
+		let sold_amount = MILION / 2;
+		let expected_amount = 0;
 		let expected_native_asset_id : TokenId = <T as Config>::NativeCurrencyId::get().into();
 		let native_asset_id : TokenId= <T as Config>::Currency::create(&caller, (2*MILION).into()).unwrap().into();
 		let non_native_asset_id : TokenId= <T as Config>::Currency::create(&caller, (2*MILION).into()).unwrap().into();
 
-		// println!("{}", T as Currency
 		assert!( expected_native_asset_id == native_asset_id );
 		assert!( non_native_asset_id != native_asset_id );
-
-		println!("{:?}", <T as Config>::Currency::free_balance(native_asset_id.into(), &caller));
-		println!("{:?}", <T as Config>::Currency::free_balance(non_native_asset_id.into(), &caller));
+		// trade pair that contains MGA but MGA is not sold token to use
+		// the most time consuming scenario of settle_and_burn
 		Xyk::<T>::create_pool(RawOrigin::Signed(caller.clone().into()).into(), native_asset_id.into(), MILION.into(), non_native_asset_id.into(), MILION.into()).unwrap();
-		// <T as Config>::Currency::mint(native_asset_id, &caller, MILION.into());
-		// <T as Config>::Currency::mint(non_native_asset_id, &caller, MILION.into());
 
-
-	}: sell_asset(RawOrigin::Signed(caller.clone().into()), native_asset_id.into(), non_native_asset_id.into(), first_asset_amount.into(), second_asset_amount.into())
+	}: sell_asset(RawOrigin::Signed(caller.clone().into()), non_native_asset_id.into(), native_asset_id.into(), sold_amount.into(), expected_amount)
 	verify {
-
-		// assert_eq!(
-		// 	Xyk::<T>::asset_pool((first_asset_id.into(), second_asset_id.into())),
-		// 	(first_asset_amount as u128, second_asset_amount as u128)
-		// );
-        //
-		// assert!(
-		// 	Xyk::<T>::liquidity_asset((first_asset_id.into(), second_asset_id.into())).is_some()
-		// );
-        //
-		// assert_eq!(
-		// 	Xyk::<T>::liquidity_pool(liquidity_asset_id),
-		// 	Some((first_asset_id.into(), second_asset_id.into()))
-		// );
-        //
-		// assert!(LiquidityMiningUser::<T>::try_get((caller.clone(), liquidity_asset_id)).is_ok());
-		// assert!(LiquidityMiningPool::<T>::try_get(liquidity_asset_id).is_ok());
-
 	}
 
 	impl_benchmark_test_suite!(Xyk, crate::mock::new_test_ext(), crate::mock::Test)

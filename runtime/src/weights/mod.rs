a8930474eb6afa7fe33494b65caa9b9d6044260f
Striker <siddiquishoeb.iitm@gmail.com>
2022-03-28 04:18:17 +0530

    fmt

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -1,12 +1,12 @@
 #![allow(clippy::unnecessary_cast)]
 
 pub mod frame_system_weights;
-pub mod pallet_session_weights;
-pub mod pallet_timestamp_weights;
 pub mod orml_tokens_weights;
-pub mod parachain_staking_weights;
-pub mod pallet_xyk_weights;
-pub mod xcm_asset_registry_weights;
-pub mod pallet_treasury_weights;
 pub mod pallet_collective_weights;
 pub mod pallet_elections_phragmen_weights;
+pub mod pallet_session_weights;
+pub mod pallet_timestamp_weights;
+pub mod pallet_treasury_weights;
+pub mod pallet_xyk_weights;
+pub mod parachain_staking_weights;
+pub mod xcm_asset_registry_weights;

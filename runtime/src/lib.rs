5e9bb56a5fb3a3a7ba053efddec708cab3b5858d
Striker <siddiquishoeb.iitm@gmail.com>
2022-05-16 12:58:06 +0530

    Using KSM_MGA_SCALE_FACTOR in bootstrap instead of 10000

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -550,7 +550,7 @@ impl pallet_bootstrap::Config for Runtime {
 	type PoolCreateApi = Xyk;
 	type Currency = orml_tokens::MultiTokenCurrencyAdapter<Runtime>;
 	type KsmToMgaRatioNumerator = frame_support::traits::ConstU128<1>;
-	type KsmToMgaRatioDenominator = frame_support::traits::ConstU128<10000>;
+	type KsmToMgaRatioDenominator = frame_support::traits::ConstU128<KSM_MGA_SCALE_FACTOR>;
 	type VestingProvider = Vesting;
 	type WeightInfo = weights::pallet_bootstrap_weights::ModuleWeight<Runtime>;
 }

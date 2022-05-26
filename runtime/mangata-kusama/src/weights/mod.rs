c9a9803fa93b770c75e3eaa97889baa4462a1822
Mateusz Nowakowski <mat13mn@gmail.com>
2022-05-26 15:24:22 +0200

    fix

---
+++ runtime/mangata-kusama/src/weights/mod.rs
@@ -0,0 +1,17 @@
+#![allow(clippy::unnecessary_cast)]
+
+pub mod frame_system_weights;
+pub mod orml_tokens_weights;
+pub mod pallet_bootstrap_weights;
+pub mod pallet_collective_weights;
+pub mod pallet_crowdloan_rewards_weights;
+pub mod pallet_elections_phragmen_weights;
+pub mod pallet_issuance_weights;
+pub mod pallet_session_weights;
+pub mod pallet_timestamp_weights;
+pub mod pallet_treasury_weights;
+pub mod pallet_utility_weights;
+pub mod pallet_vesting_mangata_weights;
+pub mod pallet_xyk_weights;
+pub mod parachain_staking_weights;
+pub mod xcm_asset_registry_weights;

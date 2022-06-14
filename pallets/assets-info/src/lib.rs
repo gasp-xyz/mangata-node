b516b0a5d388e4139b6b5cc28c847324b58c6f00
Ayush <shuchimishra20192020@gmail.com>
2022-06-14 09:00:07 +0200

    refactoring

--- pallets/assets-info/src/lib.rs
+++ pallets/assets-info/src/lib.rs
@@ -16,6 +16,7 @@ use frame_system::{ensure_root, pallet_prelude::*};
 use mangata_primitives::TokenId;
 use scale_info::TypeInfo;
 use sp_std::prelude::*;
+use sp_std::convert::TryInto;
 
 use orml_tokens::MultiTokenCurrencyExtended;
 

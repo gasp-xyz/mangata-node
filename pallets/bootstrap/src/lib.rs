9bc089837ba4d090e3da43c4abe0c9e18e1911c9
Ayush <shuchimishra20192020@gmail.com>
2022-06-14 09:04:13 +0200

    refactoring

--- pallets/bootstrap/src/lib.rs
+++ pallets/bootstrap/src/lib.rs
@@ -19,7 +19,7 @@ use sp_bootstrap::PoolCreateApi;
 use sp_core::U256;
 use sp_runtime::traits::{AccountIdConversion, CheckedAdd};
 use sp_std::prelude::*;
-use sp_std::convert::TryInto
+use sp_std::convert::TryInto;
 
 #[cfg(test)]
 mod mock;

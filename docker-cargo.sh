e65618012c021da0beb265ace4fbd9f158e88bd4
Mateusz Nowakowski <mat13mn@gmail.com>
2022-06-29 05:02:58 +0200

    provide extra env variable for injecting custom args

--- docker-cargo.sh
+++ docker-cargo.sh
@@ -69,8 +69,8 @@ docker run \
 	--name=${DOCKER_JOB_NAME} \
 	--user $DOCKER_USER \
 	-v ${REPO_ROOT}:/code \
-	-v /home/dev/substrate:/substrate \
         ${DOCKER_MOUNT_CACHE_VOLUMES} \
+        ${DOCKER_RUN_EXTRA_ARGS} \
 	-e CARGO_TARGET_DIR="/code/${OUTPUT_DIR}" \
 	${ALLOCATE_TTY_OR_NOT} ${DOCKER_BUILDER_IMAGE} \
 	cargo ${CARGO_COMMAND} --manifest-path=/code/Cargo.toml ${CARGO_ARGS}

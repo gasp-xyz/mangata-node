7b1a38d5325d11b6d9ac5811e2b872e149a7b6b4
Ayush <shuchimishra20192020@gmail.com>
2022-07-13 15:17:32 +0200

    minor refactoring

--- node/src/command.rs
+++ node/src/command.rs
@@ -374,6 +374,8 @@ pub fn run() -> Result<()> {
 						cmd.run(config, partials.client.clone(), db, storage)
 					}),
 					BenchmarkCmd::Overhead(_) => Err("Unsupported benchmarking command".into()),
+					BenchmarkCmd::Machine(cmd) => runner
+						.sync_run(|config| cmd.run(&config, SUBSTRATE_REFERENCE_HARDWARE.clone())),
 				},
 				_ => panic!("invalid chain spec"),
 			}

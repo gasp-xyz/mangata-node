efeaf37a3582ae3b3c6711c1273ea57b3a39c86f
Striker <siddiquishoeb.iitm@gmail.com>
2022-07-21 02:50:37 +0530

    Use benchmark pallet subcommand with clap

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -3,14 +3,14 @@ REPO_ROOT=$(dirname $(readlink -f $0))/../
 
 mkdir ./benchmarks
 
-${REPO_ROOT}/target/release/mangata-node benchmark-deprecated \
+${REPO_ROOT}/target/release/mangata-node benchmark pallet \
     --chain dev \
     --execution wasm \
     --wasm-execution compiled \
     --pallet $1 \
     --extrinsic '*' \
-    --steps 50 \
-    --repeat 20 \
+    --steps 2 \
+    --repeat 2 \
     --output ./benchmarks/$1_weights.rs \
     --template ./templates/module-weight-template.hbs \
     &> ./benchmarks/benchmark_$1.txt

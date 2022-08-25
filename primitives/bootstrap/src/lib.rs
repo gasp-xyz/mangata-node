3145b99fad288771e8026d5a05c849c1197ce7f8
Mateusz Nowakowski <mat13mn@gmail.com>
2022-08-25 11:24:26 +0200

    create RewardsApi used for promoting liquidity tokens in bootstrap

--- primitives/bootstrap/src/lib.rs
+++ primitives/bootstrap/src/lib.rs
@@ -24,3 +24,21 @@ pub trait PoolCreateApi {
 		second_amount: Balance,
 	) -> Option<(TokenId, Balance)>;
 }
+
+pub trait RewardsApi {
+	type AccountId: Parameter
+		+ Member
+		+ MaybeSerializeDeserialize
+		+ Debug
+		+ MaybeDisplay
+		+ Ord
+		+ MaxEncodedLen;
+
+	fn is_pool_promoted(first: TokenId, second: TokenId) -> bool;
+
+	fn activate_liquidity_tokens(
+		user: Self::AccountId,
+		liquidity_asset_id: TokenId,
+		amount: Balance,
+	) -> bool;
+}

efcf9bdf2501f2882d049bd0bb9ac6149e91f97b
Mateusz Nowakowski <mat13mn@gmail.com>
2022-12-16 20:16:16 +0100

    bump kusama version

--- runtime/mangata-kusama/src/lib.rs
+++ runtime/mangata-kusama/src/lib.rs
@@ -194,11 +194,11 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mangata-parachain"),
 	impl_name: create_runtime_str!("mangata-parachain"),
-	authoring_version: 11,
-	spec_version: 11,
+	authoring_version: 12,
+	spec_version: 12,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 11,
+	transaction_version: 12,
 	state_version: 0,
 };
 

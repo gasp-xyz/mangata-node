49566093616edcaa6ae49aa0fd99266588f08d9a
devdanco <devdanco@gmail.com>
2022-12-22 07:32:07 +0100

    chore: test runtime version

--- runtime/mangata-rococo/src/lib.rs
+++ runtime/mangata-rococo/src/lib.rs
@@ -161,11 +161,11 @@ impl_opaque_keys! {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mangata-parachain"),
 	impl_name: create_runtime_str!("mangata-parachain"),
-	authoring_version: 12,
-	spec_version: 12,
+	authoring_version: 11,
+	spec_version: 11,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 12,
+	transaction_version: 11,
 	state_version: 0,
 };
 

84ccbd5f550bea816c9c59346a9f0cb3ce7784ff
Mateusz Nowakowski <mat13mn@gmail.com>
2023-02-11 18:25:39 +0100

    use new versioning convention that matches version from Github releases

--- runtime/mangata-rococo/src/lib.rs
+++ runtime/mangata-rococo/src/lib.rs
@@ -175,10 +175,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("mangata-parachain"),
 	impl_name: create_runtime_str!("mangata-parachain"),
 	authoring_version: 14,
-	spec_version: 14,
+	spec_version: 2800,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 14,
+	transaction_version: 2800,
 	state_version: 0,
 };
 

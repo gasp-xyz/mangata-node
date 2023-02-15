fea8c51fd95615f7c4a1af36465a96b360e83969
vanderian <vanderka.marian@gmail.com>
2023-02-15 13:13:25 +0100

    [identity] format

--- runtime/mangata-rococo/src/lib.rs
+++ runtime/mangata-rococo/src/lib.rs
@@ -1650,7 +1650,7 @@ construct_runtime!(
 
 		// Bootstrap
 		Bootstrap: pallet_bootstrap::{Pallet, Call, Storage, Event<T>} = 53,
-		
+
 		Utility: pallet_utility_mangata::{Pallet, Call, Event} = 54,
 		Proxy: pallet_proxy::{Pallet, Call, Storage, Event<T>} = 55,
 		Identity: pallet_identity::{Pallet, Call, Storage, Event<T>} = 56,

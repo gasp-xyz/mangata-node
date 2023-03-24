304f26c523ac37111285c793e5fd5a9374a2eda0
mateuszaaa <mateusz@mangata.finance>
2023-03-24 13:25:44 +0100

    [WIP] 0.28.2 develop <-> main alignment (#455)
    - Trigerring change for first release branch flow. (#410)
    - Hotfix/release pipeline (#411)
    - Release/0.27.2 (#414)
    - Release/0.28.0 (#428)
    - Fix: [MGX-397] do not include txs into the block when there is no room
    for execution previous ones (#439)
    - Fix: [MGX-397] do not include txs into the block when there is no room
    for execution previous ones (#441)
    - 0.28.2 release (#450)
    - trigger ci
    
    
    [MGX-397]:
    https://mangatafinance.atlassian.net/browse/MGX-397?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [MGX-397]:
    https://mangatafinance.atlassian.net/browse/MGX-397?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    ---------
    
    Co-authored-by: Mario Kapusta <mariokapustaa@gmail.com>
    Co-authored-by: goncer <gonza.cerro@gmail.com>
    Co-authored-by: Misha Kolesnik <tenequm@gmail.com>
    Co-authored-by: iStrike7 <40371953+iStrike7@users.noreply.github.com>
    Co-authored-by: Michal Danco <devdanco@gmail.com>
    Co-authored-by: Marian Vanderka <vanderka.marian@gmail.com>

--- node/src/chain_spec/mangata_rococo.rs
+++ node/src/chain_spec/mangata_rococo.rs
@@ -148,15 +148,15 @@ pub fn mangata_rococo_prod_config() -> ChainSpec {
 						// Id of MGA token,
 						0u32,
 						// How much mangata they pool
-						10_000__000_000_000_000_000_000u128,
+						10_000_000__000_000_000_000_000_000u128,
 						// Id of the dummy token,
 						2u32,
 						// How many dummy tokens they pool,
-						20_000__000_000_000_000_000_000u128,
+						20_000_000__000_000_000_000_000_000u128,
 						// Id of the liquidity token that is generated
 						3u32,
 						// How many liquidity tokens they stake,
-						10_000__000_000_000_000_000_000u128,
+						10_000_000__000_000_000_000_000_000u128,
 					),
 					(
 						// Who gets to stake initially
@@ -164,15 +164,15 @@ pub fn mangata_rococo_prod_config() -> ChainSpec {
 						// Id of MGA token,
 						0u32,
 						// How much mangata they pool
-						8_000__000_000_000_000_000_000u128,
+						8_000_000__000_000_000_000_000_000u128,
 						// Id of the dummy token,
 						2u32,
 						// How many dummy tokens they pool,
-						20_000__000_000_000_000_000_000u128,
+						20_000_000__000_000_000_000_000_000u128,
 						// Id of the liquidity token that is generated
 						3u32,
 						// How many liquidity tokens they stake,
-						5_000__000_000_000_000_000_000u128,
+						5_000_000__000_000_000_000_000_000u128,
 					),
 				],
 				vec![
@@ -297,15 +297,15 @@ pub fn mangata_rococo_local_config() -> ChainSpec {
 						// Id of MGA token,
 						0u32,
 						// How much mangata they pool
-						10_000__000_000_000_000_000_000u128,
+						10_000_000__000_000_000_000_000_000u128,
 						// Id of the dummy token,
 						2u32,
 						// How many dummy tokens they pool,
-						20_000__000_000_000_000_000_000u128,
+						20_000_000__000_000_000_000_000_000u128,
 						// Id of the liquidity token that is generated
 						3u32,
 						// How many liquidity tokens they stake,
-						10_000__000_000_000_000_000_000u128,
+						10_000_000__000_000_000_000_000_000u128,
 					),
 					(
 						// Who gets to stake initially
@@ -313,15 +313,15 @@ pub fn mangata_rococo_local_config() -> ChainSpec {
 						// Id of MGA token,
 						0u32,
 						// How much mangata they pool
-						8_000__000_000_000_000_000_000u128,
+						8_000_000__000_000_000_000_000_000u128,
 						// Id of the dummy token,
 						2u32,
 						// How many dummy tokens they pool,
-						20_000__000_000_000_000_000_000u128,
+						20_000_000__000_000_000_000_000_000u128,
 						// Id of the liquidity token that is generated
 						3u32,
 						// How many liquidity tokens they stake,
-						5_000__000_000_000_000_000_000u128,
+						5_000_000__000_000_000_000_000_000u128,
 					),
 				],
 				vec![

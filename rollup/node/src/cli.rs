d39947ab391eb2557b783ec10cb18078a9023f3a
Mateusz Nowakowski <mat13mn@gmail.com>
2024-07-10 09:23:31 +0200

    support '--override-eth-sequencers' & '--override-arb-sequencers' for build-spec subcommand

--- rollup/node/src/cli.rs
+++ rollup/node/src/cli.rs
@@ -5,10 +5,10 @@ pub struct Cli {
 	#[command(subcommand)]
 	pub subcommand: Option<Subcommand>,
 
-	#[arg(long, value_parser, value_delimiter = ',')]
+	#[arg(long, value_parser, value_delimiter = ',', global = true)]
 	pub override_eth_sequencers: Vec<String>,
 
-	#[arg(long, value_parser, value_delimiter = ',')]
+	#[arg(long, value_parser, value_delimiter = ',', global = true)]
 	pub override_arb_sequencers: Vec<String>,
 
 	#[clap(flatten)]

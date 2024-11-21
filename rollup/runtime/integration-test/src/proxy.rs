9336d41daca8a2770414e278e924fb6427c7f33e
Marian Vanderka <vanderka.marian@gmail.com>
2024-11-21 20:18:27 +0100

    fixes tests after xyk calls removal

--- rollup/runtime/integration-test/src/proxy.rs
+++ rollup/runtime/integration-test/src/proxy.rs
@@ -29,22 +29,6 @@ fn proxy_permissions_correct() {
 			currency_id: NATIVE_ASSET_ID,
 			amount: 10 * UNIT,
 		}));
-		let provide_liquidity_call =
-			Box::new(RuntimeCall::Xyk(pallet_xyk::Call::provide_liquidity_with_conversion {
-				liquidity_asset_id: LP_ASSET_ID,
-				provided_asset_id: NATIVE_ASSET_ID,
-				provided_asset_amount: 10 * UNIT,
-			}));
-		let compound_call = Box::new(RuntimeCall::Xyk(pallet_xyk::Call::compound_rewards {
-			liquidity_asset_id: LP_ASSET_ID,
-			amount_permille: Permill::one(),
-		}));
-		let create_pool_call = Box::new(RuntimeCall::Xyk(pallet_xyk::Call::create_pool {
-			first_asset_id: NATIVE_ASSET_ID,
-			first_asset_amount: 100,
-			second_asset_id: ASSET_ID_1,
-			second_asset_amount: 100,
-		}));
 
 		// Alice's gives compound permissions to Bob
 		assert_ok!(Proxy::add_proxy(
@@ -53,27 +37,6 @@ fn proxy_permissions_correct() {
 			ProxyType::AutoCompound,
 			0
 		));
-		// Bob can be a proxy for alice compound calls
-		assert_ok!(Proxy::proxy(
-			RuntimeOrigin::signed(AccountId::from(BOB)),
-			AccountId::from(ALICE).into(),
-			Some(ProxyType::AutoCompound),
-			provide_liquidity_call.clone()
-		));
-		assert_eq!(
-			Xyk::asset_pool((NATIVE_ASSET_ID, ASSET_ID_1)),
-			(19995851032790690753, 10 * UNIT)
-		);
-
-		// Bob can be a proxy for alice compound calls
-		assert_ok!(Proxy::proxy(
-			RuntimeOrigin::signed(AccountId::from(BOB)),
-			AccountId::from(ALICE).into(),
-			Some(ProxyType::AutoCompound),
-			compound_call.clone()
-		));
-		// assert NoRewardsEvent
-
 		// Bob can't proxy for alice in a non compound call, proxy call works but nested call fails
 		assert_ok!(Proxy::proxy(
 			RuntimeOrigin::signed(AccountId::from(BOB)),
@@ -81,21 +44,13 @@ fn proxy_permissions_correct() {
 			Some(ProxyType::AutoCompound),
 			transfer_call.clone()
 		));
-		// the transfer call fails as Bob only had compound permission for alice
-		assert!(Tokens::free_balance(NATIVE_ASSET_ID, &AccountId::from(BOB)) < 1000 * UNIT);
-
-		// create pool call is part of the Xyk but is not in the AutoCompound ProxyType filter
-		assert_ok!(Proxy::proxy(
-			RuntimeOrigin::signed(AccountId::from(BOB)),
-			AccountId::from(ALICE).into(),
-			Some(ProxyType::AutoCompound),
-			create_pool_call.clone()
-		));
 		// hence the failure
 		System::assert_last_event(
 			pallet_proxy::Event::ProxyExecuted { result: Err(SystemError::CallFiltered.into()) }
 				.into(),
 		);
+		// the transfer call fails as Bob only had compound permission for alice
+		assert!(Tokens::free_balance(NATIVE_ASSET_ID, &AccountId::from(BOB)) < 1000 * UNIT);
 
 		// remove proxy works
 		assert_ok!(Proxy::remove_proxy(
@@ -109,7 +64,7 @@ fn proxy_permissions_correct() {
 				RuntimeOrigin::signed(AccountId::from(BOB)),
 				AccountId::from(ALICE).into(),
 				Some(ProxyType::AutoCompound),
-				provide_liquidity_call.clone()
+				transfer_call.clone()
 			),
 			pallet_proxy::Error::<Runtime>::NotProxy
 		);

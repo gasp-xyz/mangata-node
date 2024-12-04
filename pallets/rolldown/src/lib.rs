35152faa89a490e5d5ade08984cc71fe6fd8dfda
Marian Vanderka <vanderka.marian@gmail.com>
2024-12-04 17:49:20 +0100

    fixes seq dies on same update GASP-1675

--- pallets/rolldown/src/lib.rs
+++ pallets/rolldown/src/lib.rs
@@ -1496,7 +1496,7 @@ impl<T: Config> Pallet<T> {
 			(update.pendingDeposits.len() as u128) +
 			(update.pendingCancelResolutions.len() as u128);
 
-		ensure!(last_id > LastProcessedRequestOnL2::<T>::get(l1), Error::<T>::WrongRequestId);
+		ensure!(last_id >= LastProcessedRequestOnL2::<T>::get(l1), Error::<T>::WrongRequestId);
 
 		let mut deposit_it = update.pendingDeposits.iter();
 		let mut cancel_it = update.pendingCancelResolutions.iter();
